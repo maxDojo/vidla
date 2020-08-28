5d3ef7447e233982dd89fab632fa3a0b4d00f493
Johnson Adodo <johnsonadodo@gmail.com>
2020-08-28 10:25:32 +0100

    revisited

--- middleware/purchase.js
+++ middleware/purchase.js
@@ -1,4 +1,10 @@
+// import paystack from "paystack";
+const paystack = require("paystack")(
+  sk_test_55d48b130ad98c387fb9b9247fb18bd671b19178
+);
 // Check if payment of the approved price was successful
+
 // Update customer stats/history
 // Order shipping of the choosen movie
 // Decrement movie stock
+//
